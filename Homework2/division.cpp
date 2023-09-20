46d0c48f7dd24a1ebec7b3253a8cf55f4d72643c
Egor <68235201+Intellectual23@users.noreply.github.com>
2023-09-20 12:20:26 +0300

    Update division.cpp

--- Homework2/division.cpp
+++ Homework2/division.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
 
+int main() {
+    int n = 0, d = 0;
+    std::cin >> n >> d;
+    std::cout << n / d << " " << n % d << std::endl;
+    int q = 0, m = 1;
+    if (d < 0) {
+        d *= -1;
+        m = -1;
+    }
+    if (n > 0) {
+        while (n - d >= 0) {
+            n -= d;
+            q += m;
+        }
+    } else {
+        m *= -1;
+        while (n + d <= 0) {
+            n += d;
+            q += m;
+        }
+    }
+    std::cout << q << " " << n;
+    return 0;
+}
