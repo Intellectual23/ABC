d525e18f2a0acc923376cbefb57a889202ff75b1
Egor Kalashnikov <68235201+Intellectual23@users.noreply.github.com>
2023-10-05 18:24:33 +0300

    Update factorial.asm

--- Homework4/factorial.asm
+++ Homework4/factorial.asm
@@ -1,23 +1,23 @@
 .data 
-rep: .asciz "\n" #Строка-разделитель
+rep: .asciz "\n" #РЎС‚СЂРѕРєР°-СЂР°Р·РґРµР»РёС‚РµР»СЊ
 out: .asciz "\n max factorial in int32 is: "
 .text
-	li s2  2147483647 # Max int value
+	li t5  2147483647 # Max int value
 	li t1 1 # n = 1
 	while:
 		mv a0 t1
-		jal     fact     # Параметр в a0
-        	li      a7 1	# Вывод результата вычисления n!
-        	ecall         
+		jal     fact     # РџР°СЂР°РјРµС‚СЂ РІ a0
+        	li      a7 1	# Р’С‹РІРѕРґ СЂРµР·СѓР»СЊС‚Р°С‚Р° РІС‹С‡РёСЃР»РµРЅРёСЏ n!
+        	ecall          # РџР°СЂР°РјРµС‚СЂ СѓР¶Рµ РІ a0 ))
 		addi t1 t1 1 	#++n
-		div t2 s2 t1	#проверка, что n! < MaxInt, (n-1)! < MaxInt/n, тогда переполнения не будет
+		div t2 t5 t1	#РїСЂРѕРІРµСЂРєР°, С‡С‚Рѕ n! < MaxInt, (n-1)! < MaxInt/n, С‚РѕРіРґР° РїРµСЂРµРїРѕР»РЅРµРЅРёСЏ РЅРµ Р±СѓРґРµС‚
 		bgt a0 t2 end_while
 		la a0 rep
         	li a7 4
         	ecall
 		j while
 	end_while:
-	addi t1 t1 -1
+        addi t1 t1 -1
 	li a7 4
 	la a0 out
 	ecall
@@ -26,13 +26,19 @@ out: .asciz "\n max factorial in int32 is: "
 	ecall
         li      a7 10		#exit call
         ecall
-# Подпрограмма вычисления факториала
-fact:	li t3, 1 # счётчик t3
-       	mv t0 t3 # в t0 храним произведение
-        while_f:
-        	bgt t3 a0 end
-        	mul t0 t0 t3
-        	addi t3 t3 1
-        	j while_f
-       end: 	mv a0 t0
-       		ret
+# Р РµРєСѓСЂСЃРёРІРЅР°СЏ РїРѕРґРїСЂРѕРіСЂР°РјРјР° РІС‹С‡РёСЃР»РµРЅРёСЏ С„Р°РєС‚РѕСЂРёР°Р»Р° СЃ РёСЃРїРѕР»СЊР·РѕРІР°РЅРёРµРј s-СЂРµРіРёСЃС‚СЂР°
+fact:   addi    sp sp -8        # РІС‹РґРµР»СЏРµРј РґРІРµ СЏС‡РµР№РєРё РІ СЃС‚РµРєРµ
+        sw      ra 4(sp)        # СЃРѕС…СЂР°РЅСЏРµРј ra
+        sw      s1 (sp)         # СЃРѕС…СЂР°РЅСЏРµРј s1
+        mv      s1 a0           # s1 = n
+        addi    a0 s1 -1        # a0 = n - 1
+        li      t0 1
+        ble     a0 t0 done      # Р•СЃР»Рё n=1, РІС‹РІРѕРґРёРј
+        jal     fact            # СЂРµРєСѓСЂСЃРёРІРЅС‹Р№ РІС‹Р·РѕРІ
+        mul     s1 s1 a0 
+# Р’РѕР·РІСЂР°С‚ РёР· РїРѕРґРїСЂРѕРіСЂР°РјРјС‹ Рё РІРѕСЃСЃС‚Р°РЅРѕРІР»РµРЅРёРµ СЂРµРіРёСЃС‚СЂРѕРІ
+done:   mv      a0 s1           
+        lw      s1 (sp)         
+        lw      ra 4(sp)        
+        addi    sp sp 8
+        ret
