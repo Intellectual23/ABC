deb700214895d1481cda3464b69359015c5a3047
Egor Kalashnikov <68235201+Intellectual23@users.noreply.github.com>
2023-11-06 17:43:36 +0300

    Update code.cpp

--- IHW2/code.cpp
+++ IHW2/code.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
 #include <cmath>
 
+double f(double a, double b, double x) {
+    return a + b * std::pow(x, -2);
+}
+
 double integrate(double a, double b, double x1, double x2) {
     double n = 2; // Кол-во отрезков/интервалов
     double h = (x2 - x1) / n; // Длина каждого отрезка
-    double integral_prev = 0; // предыдущий результат
-    double integral_curr = h / 3 * (a + b * std::pow(x1, -2) + a + b * std::pow(x2, -2)); // текуший результат
-    while (std::abs(integral_curr - integral_prev) > 0.0001) { //пока не достигли нужной нам точности
-        integral_prev = integral_curr;
-        double sum1 = 0; // sum of f(x_i-1) values
-        double sum2 = 0; // sum of f(x_i-1/2) values
-        for (int i = 1; i <= n - 1; i++) {
+    double prev = 0; // предыдущий результат
+    double curr = (h / 3) * (f(a, b, x1) + f(a, b, x2)); // текуший результат
+    while (std::abs(curr - prev) > 0.0001) { //пока не достигли нужной нам точности
+        prev = curr;
+        double sum1 = 0; // первая новая сумма
+        double sum2 = 0; // вторая новая сумма
+        for (int i = 1; i <= n - 1; i++) { //суммируем
             double x = x1 + i * h; //подсчет узла
             if (i % 2 == 0) { //подсчет новых сумм
-                sum2 += a + b * std::pow(x, -2);
+                sum2 += f(a, b, x);
             } else {
-                sum1 += a + b * std::pow(x, -2);
+                sum1 += f(a, b, x);
             }
         }
-        integral_curr = integral_prev / 2 + h / 3 * (sum1 + 2 * sum2); //считаем наш интеграл по формуле.
+        curr = prev / 2 + h / 3 * (sum1 + 2 * sum2); //считаем наш интеграл по формуле.
         n *= 2; // Увеличиваем количество отрезков в 2 раза и
         h /= 2;// Соответственно уменьшаем длину каждого в два раза
     }
-
-    return integral_curr;
+    return curr;
 }
 
 int main() {
