272a65f51214d2fa03b4e577515c153903fea34b
Egor Kalashnikov <68235201+Intellectual23@users.noreply.github.com>
2023-11-06 17:41:00 +0300

    Update main.asm

--- IHW2/main.asm
+++ IHW2/main.asm
@@ -1,1 +1,12 @@
-
+.text
+.globl main
+.include "macrolib.asm"
+main:
+	read_double(fa1) #считываем a, b, x1 и x2
+	read_double(fa2)
+	read_double(fa3)
+	read_double(fa4)
+	jal integrate #вызываем подпрограмму интегрирования методом Симсона(парабол), переданы параметры a b x1 x2
+	print_double(fa0) #результат в fa0 - печатаем его
+	li a7 10 #exit
+	ecall
