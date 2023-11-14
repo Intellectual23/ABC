26bec1671a1627a25866ac7e182f72cfa8c79315
Egor Kalashnikov <68235201+Intellectual23@users.noreply.github.com>
2023-11-14 21:46:36 +0300

    Update main.asm

--- Homework6/main.asm
+++ Homework6/main.asm
@@ -1,27 +1,37 @@
 .globl main
 .include "macros.asm"
-.eqv     BUF_SIZE 10
+.eqv     BUF_SIZE 100
 .data
-buf1:    .space BUF_SIZE     # Буфер для чтения данных
-buf2:	 .space BUF_SIZE
-empty_test_str: .asciz ""   # Пустая тестовая строка
-short_test_str: .asciz "Hello!"     # Короткая тестовая строка
-long_test_str:  .asciz "I am long for BUF_SIZE" # Длинная тестовая строка
+sep: .asciz "\n"
+dest:    .space BUF_SIZE     # Буфер для чтения данных
+src: .space BUF_SIZE
+empty_str: .asciz ""   # Пустая тестовая строка
+test_str: .asciz "Hello!"     # строка символов
+.global dest
+.global src
 .text
 main:
-input_str(buf1)
-input_str(buf2)
-print_str(buf1)
-print_str(buf2)
-la a1 buf1
-la a2 buf2
-li s1 0x20
-sb s1 3(a1) #создать новый буфер! третий
+	read_str(dest) # Считывание строк с клавиатуры
+	read_str(src)
 li a7 5
 ecall
 mv a3 a0
-jal strncpy
-print_str(buf1)
-li a7 10
+
+# 1 тест - две строки с консоли
+strncpy(dest, src, a3) # Вызов макроса функции strncpy
+print_str(dest) # Печать результата
+
+# 2 тест - с пустой строкой и строкой символов
+li a3 7 
+strncpy(empty_str, test_str, a3)
+print_str(empty_str)
+li a7 4
+la a0 sep
+ecall
+# 3 тест - со строкой символов и src
+li a3 5
+strncpy(test_str, src, a3)
+print_str(test_str)
+li a7 10 # Exit
 ecall
     
