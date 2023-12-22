c624562f845d52ee6d413d12e77d460045aa9038
Egor Kalashnikov <68235201+Intellectual23@users.noreply.github.com>
2023-12-22 17:30:29 +0300

    Update main.cpp

--- IHW4/main.cpp
+++ IHW4/main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
+#include <fstream>
 #include <pthread.h>
 #include <unistd.h>
 #include <ctime>
 #include <random>
 #include <algorithm>
+#include "InputOutput.h"
 
 const int MAX_FLOWERS = 40; // По условию 40 цветков.
-const int MAX_WATERING = 40;
+int MAX_WATERING = 40;
 int wateringCount = 0;      // Счётчик политых цветов.
-pthread_mutex_t consoleMutex = PTHREAD_MUTEX_INITIALIZER; // Мьютекс для вывода, чтобы на консоли сообщения не перебивали друг друга.
-pthread_mutex_t wateringMutex = PTHREAD_MUTEX_INITIALIZER; // Мьютекс для поливкки, чтобы два садовника не поливали одновременно один и тот же цветок.
 
-void printMessage(const std::string &message) { // Печать данных на экран и в файл.
-    pthread_mutex_lock(&consoleMutex);
-    std::cout << message << std::endl;
-    pthread_mutex_unlock(&consoleMutex);
-}
+pthread_mutex_t wateringMutex = PTHREAD_MUTEX_INITIALIZER; // Мьютекс для поливкки, чтобы два садовника не поливали одновременно один и тот же цветок.
 
 int Generate(const int maxValue) { // Генерация данных от 0 до заданной границы.
     static std::mt19937 eng(std::time(NULL));
@@ -36,9 +32,9 @@ public:
 
     explicit Flower(int num) : _flowerNum(num), _flowerState(FlowerState::NeedsWatering) {}
 
-    void Water(std::string gardenerName) {
+    void Water(std::string gardenerName) { // Cмена состояния цветка при поливке.
         if (_flowerState != FlowerState::DriedOrRoten) {
-            printMessage(gardenerName + " is watering Flower #" + std::to_string(_flowerNum) + "");
+            printMessage("= " + gardenerName + " is watering Flower #" + std::to_string(_flowerNum) + "");
             if (_flowerState == NeedsWatering) {
                 _flowerState = Watered;
             } else {
@@ -46,25 +42,22 @@ public:
             }
             return;
         }
-        printMessage("Flower #" + std::to_string(_flowerNum) + " is already dead. It doesn't need any water, " +
+        printMessage("+ Flower #" + std::to_string(_flowerNum) + " is already dead. It doesn't need any water, " +
                      gardenerName + "");
     }
 
-    void Dry() {
+    void Dry() { // Сушит живой цветок.
         if (_flowerState == Watered) {
             _flowerState = NeedsWatering;
         } else if (_flowerState == NeedsWatering) {
             _flowerState = DriedOrRoten; // (Dried)
         }
     }
-
-private:
-
 };
 
-Flower *flowers[MAX_FLOWERS];
+Flower *flowers[MAX_FLOWERS]; // Массив цветов, глобальный, чтобы у садовников и клумбы был к ним доступ
 
-void initFlowers() {
+void initFlowers() { //инициализация цветов от 1 до 40
     for (int i = 0; i < MAX_FLOWERS; ++i) {
         flowers[i] = new Flower(i + 1);
     }
@@ -76,11 +69,7 @@ public:
     pthread_t thread{}; // его поток.
 
     Gardener(std::string gardenerName) : _name(std::move(gardenerName)) {
-        pthread_create(&thread, NULL, &Gardener::WrappedWatering, this);
-    }
-
-    ~Gardener() {
-        pthread_join(thread, NULL);
+        pthread_create(&thread, NULL, &Gardener::WrappedWatering, this); // инициализация потока
     }
 
     static void *WrappedWatering(void *gardener) {
@@ -100,6 +89,11 @@ public:
         }
         pthread_exit(NULL);
     }
+
+    ~Gardener() {
+        pthread_join(thread, NULL); // Запуск потока.
+    }
+
 
 private:
     Flower *ChooseFlower() {
@@ -123,42 +117,44 @@ class Garden { // Класс клумбы.
 public:
     pthread_t thread{}; // ее поток
 
+    Garden() {
+        pthread_create(&thread, NULL, &Garden::MonitorFlowers, this);// Инициализация потока
+    }
 
-    static void *MonitorFlowers(void *arg) {
+    static void *MonitorFlowers(void *arg) { // Вывод информации о каждом цветке + запуск засухи
         Garden *garden = (Garden *) arg;
         usleep(100000);
         while (wateringCount < MAX_WATERING) {
             for (int i = 0; i < MAX_FLOWERS; ++i) {
                 Flower *flower = flowers[i];
                 if (flower->_flowerState == NeedsWatering) {
-                    printMessage("-Flower #" + std::to_string(flower->_flowerNum) + " needs watering!");
+                    printMessage("- Flower #" + std::to_string(flower->_flowerNum) + " needs watering!");
                 } else if (flower->_flowerState == DriedOrRoten) {
-                    printMessage("-Flower #" + std::to_string(flower->_flowerNum) + " is dried or roten :(");
+                    printMessage("- Flower #" + std::to_string(flower->_flowerNum) + " is dried or roten :(");
                 } else {
-                    printMessage("-Flower #" + std::to_string(flower->_flowerNum) + " is watered :)");
+                    printMessage("- Flower #" + std::to_string(flower->_flowerNum) + " is watered :)");
                 }
             }
-            usleep(10000000);
-            for (int i = 0; i < MAX_FLOWERS; ++i) {
+            usleep(10000000); // Ждём 10 секунд перед следующей засухой
+            for (int i = 0; i < MAX_FLOWERS; ++i) { // Засуха!
                 flowers[i]->Dry(); // Вызов метода Dry() для всех цветов
             }
         }
         return NULL;
     }
 
-    Garden() {
-        pthread_create(&thread, NULL, &Garden::MonitorFlowers, this);
-    }
-
     ~Garden() {
-        pthread_join(thread, NULL);
+        pthread_join(thread, NULL);// Запуск потока
     }
 };
 
-int main() {
-    initFlowers();
-    Garden garden;
-    Gardener mario = Gardener("Mario");
+int main(int argc, char *argv[]) {
+    // Установка кодировки консоли
+    MAX_WATERING = InputCMD(argc, argv);// по умолчанию 40 поливок, и выходной файл output.txt
+    initFlowers();// Заполняем массив цветов цветами.
+    Garden garden; // Создали Клумбу, в ней создан поток с мониторингом.
+    Gardener mario = Gardener("Mario"); // Создаем двух садовников с потоками-поливками.
     Gardener luigi = Gardener("Luigi");
+
     return 0;
 }
